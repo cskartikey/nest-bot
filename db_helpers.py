2bbc8e28926854d0b94cbbc3301acaef5b43d080
Kartikey S. Chauhan <skartikey314@gmail.com>
2023-09-28 01:57:10 +0530

    Added db_helpers.py

---
+++ db_helpers.py
@@ -0,0 +1,42 @@
+def get_username(user_id):
+    cursor.execute(
+        "SELECT tilde_username FROM nest_bot.users WHERE slack_user_id = %s", [user_id]
+    )
+    username = cursor.fetchone()[0]
+    if username != None:
+        return str(username)
+    else:
+        return None
+
+
+def get_full_name(user_id):
+    cursor.execute(
+        "SELECT name FROM nest_bot.users WHERE slack_user_id = %s", [user_id]
+    )
+    name = cursor.fetchone()[0]
+    if name != None:
+        return str(name)
+    else:
+        return None
+
+
+def get_email(user_id):
+    cursor.execute(
+        "SELECT email FROM nest_bot.users WHERE slack_user_id = %s", [user_id]
+    )
+    email = cursor.fetchone()[0]
+    if email != None:
+        return str(email)
+    else:
+        return None
+
+
+def get_ssh_key(user_id):
+    cursor.execute(
+        "SELECT ssh_key FROM nest_bot.users WHERE slack_user_id = %s", [user_id]
+    )
+    ssh_key = cursor.fetchone()[0]
+    if ssh_key != None:
+        return str(ssh_key)
+    else:
+        return None
